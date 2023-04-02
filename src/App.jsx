b40ae8a6567600be39961ef856923a2584ee0c4f
Pranay <pranayguptamain@gmail.com>
2023-04-02 19:45:09 +0530

    up

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="flex justify-center gap-4 flex-col min-h-screen">
+      <h1 className="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
+      <p className="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
+      <img src="https://bit.ly/3wsmzTy" alt="meme" className="mx-auto" />
+    </main>
+  );
+}
+
+export default App;
